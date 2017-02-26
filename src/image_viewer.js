77b547bb30ac61a378e39508d7b673c31bb597fd
Caique Caleiro <caiquecaleiro@gmail.com>
2017-02-26 16:06:33 -0300

    Add code splitting

--- src/image_viewer.js
+++ src/image_viewer.js
@@ -2,12 +2,9 @@ import big from '../assets/big.jpg';
 import small from '../assets/small.jpg';
 import '../styles/image_viewer.css';
 
-const image = document.createElement('img');
-image.src = small;
+export default () => {
+  const image = document.createElement('img');
+  image.src = small;
 
-document.body.appendChild(image);
-
-const image2 = document.createElement('img');
-image2.src = big;
-
-document.body.appendChild(image2);
+  document.body.appendChild(image);
+};
